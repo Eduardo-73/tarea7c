f82b6e20d50df29f2da574f5f287dd3ebcd45b86
Eduardo <eduardocorreaaranda10@gmail.com>
2024-03-21 21:55:54 +0100

    Mejora de ejercicio y finalización

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -11,7 +11,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 /**
@@ -22,55 +24,21 @@ public class Main {
 
     public static void main(String[] args) {
         String fichero = "personas.csv";
-        List<String> listaFichero = leerFichero(fichero);
+        System.out.println("Lectura del Fichero");
+        List<String> listaFichero = ClaseFicheros.leerFichero(fichero);
         for (String leer : listaFichero) {
             System.out.println(leer);
         }
-        List<Persona> lista = crearListaPersonas(listaFichero);
+        System.out.println("Creacion de la lista de Personas");
+        List<Persona> lista = ClaseFicheros.crearListaPersonas(listaFichero);
         for (Persona persona : lista) {
             System.out.println(persona);
         }
-        Set<String> genero = conjuntoGenero(listaFichero);
+        System.out.println("Conjunto de Géneros");
+        Set<String> genero = ClaseFicheros.conjuntoGenero(listaFichero);
         genero.forEach(System.out::println);
-    }
-
-    private static List<String> leerFichero(String fichero) {
-        List<String> lista = new ArrayList();
-        try {
-            lista = Files.readAllLines(Paths.get(fichero));
-            lista.remove(0);
-        } catch (IOException ioe) {
-            System.out.println("Error, No se a leido el fichero");
-        }
-        return lista;
-    }
-
-    private static List<Persona> crearListaPersonas(List<String> fichero) {
-        DateTimeFormatter formatted = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        List<Persona> lista = new ArrayList();
-        for (String persona : fichero) {
-            String[] array = persona.split(",");
-            LocalDate fecha = LocalDate.parse(array[5], formatted);
-            boolean jubilado = Boolean.parseBoolean(array[6]);
-            Persona nuevo = new Persona(Integer.parseInt(array[0]), array[1],
-                    array[2], array[3], array[4], fecha,
-                    jubilado, array[7]);
-            lista.add(nuevo);
-        }
-        return lista;
-    }
-
-    private static Set<String> conjuntoGenero(List<String> fichero) {
-        Set<String> lista = new TreeSet();
-        for (String genero : fichero) {
-            String[] array = genero.split(",");
-            lista.add(array[4]);
-        }
-        try {
-            Files.write(Paths.get("genero.txt"), lista);
-        } catch (IOException ioe) {
-            System.out.println("No se ha podido crear el .txt");
-        }
-        return lista;
+        System.out.println("Creación Map");
+        Map<String, Integer> map = ClaseFicheros.contadorGeneros(listaFichero);
+        map.forEach((llave, valor) -> System.out.println(llave + ", " + valor));
     }
 }
